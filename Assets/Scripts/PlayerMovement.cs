4f982319f24b5efd47614b3627314ffd511cb3df
Vikrant Kataria <vik7am@gmail.com>
2023-04-30 21:10:12 +0530

    Platform and player with first person camera

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+    private CharacterController characterController;
+    private float horizontalInput;
+    private float verticalInput;
+    private Vector3 movementDirection;
+
+    private void Awake() {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update(){
+        UpdatePlayerMovement();
+    }
+
+    private void UpdatePlayerMovement(){
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        movementDirection = transform.forward*verticalInput + transform.right*horizontalInput;
+        movementDirection = movementDirection.normalized;
+        characterController.SimpleMove(movementDirection * movementSpeed);
+    }
+}
